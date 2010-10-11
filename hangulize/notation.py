fa21a4de32249b8ac0e1c2e34b7d99b17b35d38e
Lee Heung-sub <heung@sublee.kr>
2010-10-12 00:17:07 +0900

    Initialize the project.

---
+++ hangulize/notation.py
@@ -0,0 +1,21 @@
+import re
+
+
+class Notation(object):
+
+    vowels = ()
+    rule = []
+
+    def items(self):
+        """Yields each notation rules as regex."""
+        for pattern, val in self.rule:
+            yield self.regexify(pattern), val
+
+    def regexify(self, pattern):
+        """Compiles a regular expression from the notation pattern."""
+        vowels = '|'.join([re.escape(v) for v in self.vowels])
+        regex = re.sub('@', '{%s}' % vowels, pattern)
+        regex = re.sub('^{([^}]+?)}', r'(?<=[\1])', regex)
+        regex = re.sub('{([^}]+?)}$', r'(?=[\1])', regex)
+        return re.compile(regex)
+
---
+++ hangulize/phoneme.py
@@ -0,0 +1,32 @@
+from .hangul import *
+
+
+class Phoneme(object):
+
+    def __init__(self, letter):
+        self.letter = letter
+
+    def __repr__(self):
+        return "<%s '%s'>" % (type(self).__name__, self.letter.encode('utf-8'))
+
+
+class Choseong(Phoneme):
+    pass
+
+class Jungseong(Phoneme):
+    pass
+
+class Jongseong(Phoneme):
+    pass
+
+
+def phonemes(word):
+    result = []
+    for c in word:
+        c = split(c)
+        result.append(Choseong(c[0]))
+        result.append(Jungseong(c[1]))
+        if c[2] is not Null:
+            result.append(Jongseong(c[2]))
+    return tuple(result)
+
