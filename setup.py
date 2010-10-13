fab8f1a3e961d2ecb2e928f5e37828de92986a8b
Lee Heung-sub <heung@sublee.kr>
2010-10-13 13:50:22 +0900

    `repl` 명령어 추가

--- setup.py
+++ setup.py
@@ -21,6 +21,49 @@ Links
 
 """
 from setuptools import setup
+from distutils.cmd import Command
+
+
+class repl(Command):
+    """Read-eval-print loop for Hangulize
+
+        $ python setup.py repl
+        Select Locale: it
+        ==> gloria
+        -> 'gloria'
+        -> ' loria'
+        -> '  oria'
+        -> '  o ia'
+        -> '  o i '
+        -> '  o   '
+        -> '      '
+        글로리아
+    """
+
+    user_options = [('locale=', 'l', 'the locale')]
+
+    def initialize_options(self):
+        self.locale = None
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import logging
+        from hangulize import hangulize
+        logger = logging.getLogger('Hangulize REPL')
+        logger.setLevel(logging.INFO)
+        logger.addHandler(logging.StreamHandler())
+        def _repl():
+            locale = self.locale or raw_input('Select Locale: ')
+            while True:
+                string = raw_input('==> ')
+                if not string:
+                    break
+                yield hangulize(string.decode('utf-8'),
+                                locale, logger=logger).encode('utf-8')
+        for hangul in _repl():
+            print hangul
 
 
 def run_tests():
@@ -51,6 +94,7 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Linguistic'
-    ]
+    ],
+    cmdclass={'repl': repl}
 )
 
