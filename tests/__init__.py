4957788379be8e7f6471d7c23b2f625d1eda2255
Heungsub Lee <heung@sublee.kr>
2010-12-13 17:07:42 +0900

    전체 테스트 시 메모리 사용효율 향상

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,13 @@
 import sys
 import os.path
 import unittest
+import time
+
+
+class LazyTestSuite(unittest.TestSuite):
+
+    def __init__(self, tests=()):
+        self._tests = tests
 
 
 class HangulizeTestCase(unittest.TestCase):
@@ -8,13 +15,15 @@ class HangulizeTestCase(unittest.TestCase):
     def hangulize(self, word):
         return self.lang.hangulize(word)
 
+    def tearDown(self):
+        time.sleep(0.1)
+
 
 def filename(path):
     return os.path.sep.join(os.path.basename(path).split(os.path.extsep)[:-1])
 
 
 def suite(code=None):
-    suite = unittest.TestSuite()
     loader = unittest.TestLoader()
     if code:
         mods = [code.replace('.', '_')]
@@ -22,7 +31,10 @@ def suite(code=None):
         mods = (filename(x) for x in os.listdir(os.path.dirname(__file__)) \
                             if x.endswith(os.path.extsep + 'py') and \
                                '__init__' not in x)
-    for mod in mods:
-        mod = getattr(__import__('%s.%s' % (__name__, mod)), mod)
-        suite.addTest(loader.loadTestsFromModule(mod))
+    def add_tests():
+        for mod in mods:
+            mod = getattr(__import__('%s.%s' % (__name__, mod)), mod)
+            yield loader.loadTestsFromModule(mod)
+            del mod
+    suite = LazyTestSuite(add_tests())
     return suite
