3603989d86d995e0bf315f7d8a5d3d410c67466b
iceager <iceager@gmail.com>
2015-03-24 23:58:03 +0100

    Update __init__.py
    lm, ln 등에서 l이 'ㄹㄹ'로 표기되도록 규칙 수정

--- hangulize/langs/deu/__init__.py
+++ hangulize/langs/deu/__init__.py
@@ -106,7 +106,7 @@ class German(Language):
         ('{ai|oi|au}k',   (Choseong(K),)),
         ('{@}k{c|s|S|t}', (Jongseong(G),)),
         ('k',             (Choseong(K),)),
-        ('{@}l{@}',       (Jongseong(L), Choseong(L))),
+        ('{@}l{@|m|n}',   (Jongseong(L), Choseong(L))),
         ('^l',            (Choseong(L),)),
         ('l{@}',          (Jongseong(L), Choseong(L))),
         ('l',             (Jongseong(L),)),
