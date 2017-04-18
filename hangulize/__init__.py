076918cf93720c079584940dc20e1d30661c064e
Heungsub Lee <sub@subl.ee>
2017-04-19 03:27:24 +0900

    Test on Python 3

--- hangulize/__init__.py
+++ hangulize/__init__.py
@@ -124,4 +124,5 @@ for name in ['.models', '.normalization', '.processing']:
     module = importlib.import_module(name, __name__)
     __all__.extend(module.__all__)
     for attr in module.__all__:
+        print(attr)
         locals()[attr] = getattr(module, attr)
